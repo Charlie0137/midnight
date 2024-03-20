c39df4c42a58cb0e08e50bae1c764628d1fcc601
Charlie Burrow <charlieburrow@duck.com>
2024-03-20 18:45:55 -0500

    Integrated player into Checkpoint and fixed some bugs

--- source/Checkpoint.java
+++ source/Checkpoint.java
@@ -7,12 +7,15 @@
  */
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Checkpoint {
     static ArrayList<Card> deck = new ArrayList<Card>();
-    static Player player = new Player("Example Player");
 
     public static void main(String[] args) {
+
+        Player player = new Player("Example Player");
+
         // populate the deck
         for (Card.Suits i : Card.Suits.values()) {
             for (int j = 1; j <= 13; j++) {
@@ -21,16 +24,42 @@ public class Checkpoint {
             }
             System.out.println();
         }
-        for (int i = 0; i < 52; i++) {
-            Card drawnCard = drawCard();
-            System.out.printf("%2d: %2d %s\n", i + 1, drawnCard.getValue(), drawnCard.getSuit());
-        }
+
+        playerTurn(player);
+
     }
 
     private static Card drawCard() {
+        if (deck.size() == 0) {
+            System.out.println("Error Deck is Empty");
+            System.exit(-1);
+        }
         int drawNum = (int) (Math.random() * deck.size());
         Card card = deck.get(drawNum);
         deck.remove(card);
         return card;
+    }
+
+    private static void playerTurn(Player player) {
+        Scanner userTurnScanner = new Scanner(System.in);
+        String userDirection;
+        Card drawnCard;
+
+        while (true) {
+            System.out.print("Draw a Card or Stay?\n>>> ");
+            userDirection = userTurnScanner.next();
+            if (userDirection.equalsIgnoreCase("draw") || userDirection.equalsIgnoreCase("d")) {
+
+                drawnCard = drawCard();
+                if (!player.drawCard(drawnCard))
+                    break;
+
+            } else if (userDirection.equalsIgnoreCase("stay") || userDirection.equalsIgnoreCase("s"))
+                break;
+            else
+                continue;
+            System.out.println();
+        }
+        userTurnScanner.close();
     }
 }
--- source/Player.java
+++ source/Player.java
@@ -47,40 +47,41 @@ public class Player {
      * if not, returns false
      */
     boolean drawCard(Card drawnCard) {
+        Scanner userIn = new Scanner(System.in);
 
         // tell the player what they drew
         System.out.printf("You drew %s\n", drawnCard.getCardString());
 
         // check for kings
         if (drawnCard.getValue() == 13) {
-            Scanner userIn = new Scanner(System.in);
 
             // ask the player where they wish to put the king
             System.out.print("Where would you like to place your King?\nOpen Slot(s): ");
             for (int i = 0; i < queue.length; i++) {
-                if (queue[i] != null)
-                    System.out.print(i + " ");
+                if (queue[i] == null)
+                    System.out.print(i + 1 + " ");
             }
-            System.out.println(">>>");
+            System.out.println();
 
             // verify that the users selection is open
             int userSelection;
             do {
+                System.out.print(">>> ");
                 userSelection = userIn.nextInt();
-                System.out.println();
-            } while (userSelection <= 13 && queue[userSelection] == null);
-            userIn.close();
+            } while (userSelection <= 0 || userSelection >= 13 || queue[userSelection - 1] != null);
+
+            System.out.println();
 
             // add the king to the queue
-            queue[userSelection] = drawnCard;
+            queue[userSelection - 1] = drawnCard;
             return true;
 
-        } else if (queue[drawnCard.getValue()] != null) {
+        } else if (queue[drawnCard.getValue() - 1] != null) {
 
             System.out.println("There is no Room in the Queue");
             return false;
         } else {
-            queue[drawnCard.getValue()] = drawnCard;
+            queue[drawnCard.getValue() - 1] = drawnCard;
             return true;
         }
     }
